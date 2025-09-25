3b659896da1cada53c7c79e16b1d4c69503c223b
sazzadrupak <sazzad@oployeelabs.com>
2025-09-25 19:44:05 +0300

    Test user reservations component

--- base-concert-venue/__tests__/__mocks__/msw/handlers.js
+++ base-concert-venue/__tests__/__mocks__/msw/handlers.js
@@ -1,10 +1,16 @@
 import { rest } from 'msw';
 
 import { readFakeData } from '@/__tests__/__mocks__/fakeData';
+import { fakeUserReservations } from '@/__tests__/__mocks__/fakeData/userReservations';
 
 export const handlers = [
   rest.get('http://localhost:3000/api/shows/:showId', async (req, res, ctx) => {
     const { fakeShows } = await readFakeData();
     return res(ctx.json({ show: fakeShows[0] }));
   }),
+  rest.get(
+    'http://localhost:3000/api/users/:userId/reservations',
+    async (req, res, ctx) =>
+      res(ctx.json({ userReservations: fakeUserReservations }))
+  ),
 ];
---
+++ base-concert-venue/__tests__/ui/user-reservations.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+
+import { UserReservations } from '@/components/user/UserReservations';
+
+test("Displays reservations and 'purchase more' button when reservations exist", async () => {
+  render(<UserReservations userId={1} />);
+
+  const purchaseButton = await screen.findByRole('button', {
+    name: /purchase more tickets/i,
+  });
+
+  expect(purchaseButton).toBeInTheDocument();
+});
