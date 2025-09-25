a4f7ca4b333be5a02892901e2b8ad86c005b5f8f
sazzadrupak <sazzad@oployeelabs.com>
2025-09-25 19:06:37 +0300

    Reservation component test with Mock Service Worker

--- base-concert-venue/__tests__/__mocks__/msw/handlers.js
+++ base-concert-venue/__tests__/__mocks__/msw/handlers.js
@@ -1,1 +1,10 @@
-export const handlers = [];
+import { rest } from 'msw';
+
+import { readFakeData } from '@/__tests__/__mocks__/fakeData';
+
+export const handlers = [
+  rest.get('http://localhost:3000/api/shows/:showId', async (req, res, ctx) => {
+    const { fakeShows } = await readFakeData();
+    return res(ctx.json({ show: fakeShows[0] }));
+  }),
+];
