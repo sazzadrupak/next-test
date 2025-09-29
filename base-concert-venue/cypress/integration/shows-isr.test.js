33e1eb2bb1deac209a45420f89da31b35404b4d0
sazzadrupak <sazzad@oployeelabs.com>
2025-09-29 19:39:49 +0300

    FIrst ISR cache test

---
+++ base-concert-venue/cypress/integration/shows-isr.test.js
@@ -0,0 +1,11 @@
+it('skips client-side bundle, confirming data from ISR cache', () => {
+  cy.request('/shows')
+    .its('body')
+    .then((html) => {
+      // remove the scripts, so they don't start automatically
+      const staticHtml = html.replace(/<script.*?>.*?<\/script>/gm, '');
+      cy.state('document').write(staticHtml);
+    });
+
+  cy.findAllByText(/2022 apr 1[456]/i).should('have.length.of', 3);
+});
