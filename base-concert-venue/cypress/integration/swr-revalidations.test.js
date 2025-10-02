8fa7a620b54ce9f5144b8e0f7d72f86163644032
Sazzad Rupak <sazzadrupak@Mac.home>
2025-10-02 11:31:31 +0300

    Revalidate on interval reservation page test

--- base-concert-venue/cypress/integration/swr-revalidations.test.js
+++ base-concert-venue/cypress/integration/swr-revalidations.test.js
@@ -2,6 +2,7 @@ import { generateNewReservation } from '../../__tests__/__mocks__/fakeData/newRe
 import { generateRandomId } from '../../lib/features/reservations/utils';
 
 const ONE_SECOND = 1000;
+const FIFTEEN_SECONDS = 15 * ONE_SECOND;
 const THIRTY_SECONDS = 30 * ONE_SECOND;
 
 it('should refresh the shows page after 30 seconds', () => {
@@ -25,3 +26,27 @@ it('should refresh the shows page after 30 seconds', () => {
     cy.tick(THIRTY_SECONDS);
     cy.findAllByText(/sold out/i).should('have.length', 2);
 });
+
+it('should refresh the reservation page after 15 seconds', () => {
+    cy.clock();
+    cy.task('db:reset').visit('/reservations/0');
+
+    cy.findByRole('main').within(() => 
+        cy.findByRole('button', { name: /sign in/i }).click()
+    )
+
+    cy.findByText(/10 seats left/i).should('exist');
+
+    const newReservation = generateNewReservation({
+        reservationId: 12345,
+        showId: 0,
+        seatCount: 2,
+    });
+    cy.task('addReservation', newReservation)
+
+    cy.tick(ONE_SECOND);
+    cy.findByText(/10 seats left/i).should('exist');
+
+    cy.tick(FIFTEEN_SECONDS);
+    cy.findByText(/8 seats left/i).should('exist');
+})
